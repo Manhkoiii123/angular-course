e0588bcec64f45275730df9128d52c9f7c423810
manhtranduc <manhtranduc0202@gmail.com>
2025-03-11 19:23:12 +0700

    event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,16 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
+  addToCart: number = 0;
+  name: string = 'manh';
   product = {
     name: 'Laptop',
     price: 100000,
     color: 'black',
     discount: 10,
-    inStock: 0,
+    inStock: 10,
   };
   getDiscountPrice() {
     return (
       this.product.price - this.product.price * (this.product.discount / 100)
     );
+  }
+  onNameChange(event: any) {
+    this.name = event.target.value;
+  }
+  decrement() {
+    if (this.addToCart == 0) {
+      return;
+    }
+    this.addToCart--;
+  }
+  increment() {
+    if (this.addToCart == this.product.inStock) {
+      return;
+    }
+    this.addToCart++;
   }
 }
